a1b51d8e1d0aba08a6c412daaa6ad4f5a89e5d87
Gabi Schiopu <gabi.schiopu@gmail.com>
2020-10-14 10:40:45 +0300

    test headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,11 +70,6 @@ module.exports = {
                     "Access-Control-Allow-Origin: https://ove.ro ",
                 ],
             },                                  
-            allPageHeaders: [],                           // option to add headers for all pages. `Link` headers are transformed by the below criteria
-            mergeSecurityHeaders: true,                   // boolean to turn off the default security headers
-            mergeLinkHeaders: false,                      // boolean to turn off the default gatsby js headers (disabled by default, until gzip is fixed for server push)
-            mergeCachingHeaders: true,                    // boolean to turn off the default caching headers
-            transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
         }
     }
     
